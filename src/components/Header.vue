57a6c2df7c8f73e048302b691aebd1f3891566ff
JeffRisberg <jrisberg@aol.com>
2018-03-17 17:22:02 -0700

    fixed a typo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,7 @@
             <a class="navbar-brand" href="/">Vue/Vuex Example</a>
             <ul class="navbar-nav">
                 <router-link :to="{ name: 'homeLink' }" tag="li"><a class="nav-link">home</a></router-link>
-                <router-link :to="{ name: 'menuLink' }"><a class="nav-link">menu</a></router-link>
+                <router-link :to="{ name: 'menuLink' }" tag="li"><a class="nav-link">menu</a></router-link>
             </ul>
         </nav>
     </header>
